75aefc6a55b10738a5d3e5d3d9a8e37bdfc049ce
adipotnis <50766264+adipotnis@users.noreply.github.com>
2020-10-05 19:45:59 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,82 @@
+# checks video and uploads it to corresponding folder
+import re
+import cv2
+import os
+import pytesseract
+from pytesseract import Output
+import sys
+import shutil
+#print('Argument List:', str(sys.argv))_______________________________________________________________
+
+# get grayscale image
+def get_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# filename to be accessed
+filename = str(sys.argv[1])
+
+
+#convert video beginning to image______________________________________________________________________
+
+# Read the video from specified path
+cam = cv2.VideoCapture(filename)
+
+try:
+    # creating a folder named data
+    if not os.path.exists('data'):
+        os.makedirs('data')
+
+# if not created then raise error
+except OSError:
+    print ('Error Creating directory')
+
+frame = 26
+
+# reading from frame
+ret,frame = cam.read()
+
+if ret:
+
+    name = 'stillimage.png'
+    # writing the extracted images
+    cv2.imwrite(name, frame)
+
+# Release all space and windows once done
+cam.release()
+cv2.destroyAllWindows()
+
+# get image and scan to find init text to sort__________________________________________________________
+
+img = cv2.imread('stillimage.png')
+custom_config = r'--oem 3 --psm 6'
+pytesseract.image_to_string(img, config=custom_config)
+
+gray = get_grayscale(img)
+resized = cv2.resize(gray, (600,300), interpolation = cv2.INTER_AREA)
+
+d = pytesseract.image_to_data(gray, output_type=Output.DICT)
+textlist = d['text']
+textstr = ''.join(textlist)
+#print(textlist)
+
+subject_dict ={'sbp.mech@':'OR' , 'Tapase':'IFP', 'Kadam':'ICE', 'bhavikatti':'TOM'}
+
+prof_list = ['sbp.mech@' , 'Tapase','KADAM', 'Kadam', 'bhavikatti']
+val = 0
+for i in prof_list:
+    for val, j in enumerate(textlist):
+        if j == 'UTC':
+            date_ind = val -2
+            break
+
+
+
+    if i in textstr:
+        dest_folder = "/Users/adityapotnis/Desktop/Sem5/Screen_recordings/" + subject_dict[i]+"/" + subject_dict[i] + textlist[date_ind]+'.mp4'
+        if not os.path.exists(dest_folder):
+            os.makedirs(dest_folder)
+
+        shutil.move(filename, dest_folder)
+
+        print('{} is present'.format(i))
+        break
